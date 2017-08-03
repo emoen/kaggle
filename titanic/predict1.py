b8ab15da3047a0e59afb1a5c46046affbc529a3c
Moen Endre <endre.moen@imr.no>
2017-08-03 12:49:42 +0200

    print predictionForrest

--- titanic/predict1.py
+++ titanic/predict1.py
@@ -8,7 +8,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import tree
 from sklearn.tree import export_graphviz
 from sklearn.externals.six import StringIO  
-import pydot 
+#import pydot 
+import pydotplus
 
 # Load the train and test datasets to create two DataFrames
 train_url = "train.csv"
@@ -18,42 +19,41 @@ test_url = "test.csv"
 test = pd.read_csv(test_url);
 
 # Convert the male and female groups to integer form
-train["Sex"][train["Sex"] == "male"] = 0
-train["Sex"][train["Sex"] == "female"] = 1
+train.loc[ train["Sex"] == "male", "Sex" ] = 0
+train.loc[ train["Sex"] == "female", "Sex" ] = 1
 
 train["Age"] = train["Age"].fillna(train["Age"].median())
 train["Embarked"] = train["Embarked"].fillna("S")
 
 # Convert the Embarked classes to integer form
-train["Embarked"][train["Embarked"] == "S"] = 0
-train["Embarked"][train["Embarked"] == "C"] = 1
-train["Embarked"][train["Embarked"] == "Q"] = 2
+train.loc[ train["Embarked"] == "S", "Embarked"] = 0
+train.loc[ train["Embarked"] == "C", "Embarked"] = 1
+train.loc[ train["Embarked"] == "Q", "Embarked"] = 2
 
 # Fare bin
-print(type(train))
 train["FareBin"] = train["Fare"]
-train["FareBin"][train["Fare"] < 10] = 0
-train["FareBin"][np.logical_and(train["Fare"] >= 10, train["Fare"] < 20) ] = 1
-train["FareBin"][np.logical_and(train["Fare"] >= 20, train["Fare"] < 30) ] = 2
-train["FareBin"][train["Fare"] >= 30] = 3
+train.loc[train["Fare"] < 10, "Fare"] = 0
+train.loc[np.logical_and(train["Fare"] >= 10, train["Fare"] < 20), "Fare" ] = 1
+train.loc[np.logical_and(train["Fare"] >= 20, train["Fare"] < 30), "Fare" ] = 2
+train.loc[train["Fare"] >= 30, "Fare"] = 3
 train["FareBin"] = train["FareBin"].fillna(0)
 
 # *********** test ********
-test["Sex"][test["Sex"] == "male"] = 0
-test["Sex"][test["Sex"] == "female"] = 1
+test.loc[test["Sex"] == "male", "Sex"] = 0
+test.loc[test["Sex"] == "female", "Sex"] = 1
 
 test["Age"] = test["Age"].fillna(test["Age"].median())
 test["Embarked"] = test["Embarked"].fillna("S")
 
-test["Embarked"][test["Embarked"] == "S"] = 0
-test["Embarked"][test["Embarked"] == "C"] = 1
-test["Embarked"][test["Embarked"] == "Q"] = 2
+test.loc[test["Embarked"] == "S", "Embarked"] = 0
+test.loc[test["Embarked"] == "C", "Embarked"] = 1
+test.loc[test["Embarked"] == "Q", "Embarked"] = 2
 
 test["FareBin"] = test["Fare"]
-test["FareBin"][test["Fare"] < 10] = 0
-test["FareBin"][np.logical_and(test["Fare"] >= 10, test["Fare"] < 20) ] = 1
-test["FareBin"][np.logical_and(test["Fare"] >= 20, test["Fare"] < 30) ] = 2
-test["FareBin"][test["Fare"] > 30] = 3
+test.loc[test["Fare"] < 10, "Fare"] = 0
+test.loc[np.logical_and(test["Fare"] >= 10, test["Fare"] < 20), "Fare" ] = 1
+test.loc[np.logical_and(test["Fare"] >= 20, test["Fare"] < 30), "Fare" ] = 2
+test.loc[test["Fare"] > 30, "Fare"] = 3
 test["FareBin"] = test["FareBin"].fillna(0)
 #****************
 
@@ -81,7 +81,7 @@ i_tree = 0
 for tree_in_forest in my_forest.estimators_:    
     dot_data = StringIO()
     tree.export_graphviz(tree_in_forest, out_file = dot_data)
-    graph = pydot.graph_from_dot_data(dot_data.getvalue())
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
     f_name = 'tree_' + str(i_tree) + '.pdf'
     graph.write_pdf(f_name) 
     i_tree += 1
