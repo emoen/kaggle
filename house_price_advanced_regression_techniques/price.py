d5ebeb983ffab356f13d2304055aa9bc1a584f23
emoen <endremoen@hotmail.com>
2017-10-03 20:56:40 +0200

    'A change in perspective is worth 80 IQ points' - Alan Curtis Kay

---
+++ house_price_advanced_regression_techniques/price.py
@@ -0,0 +1,78 @@
+#https://www.kaggle.com/pmarcelino/comprehensive-data-exploration-with-python
+#invite people for the Kaggle party
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+
+warnings.filterwarnings('ignore')
+#%matplotlib inline
+
+train_url = "train.csv"
+df_train = pd.read_csv(train_url)
+
+#missing data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+
+#print(missing_data.head(20))
+
+sns.distplot(df_train['SalePrice']);
+plt.show();
+
+#skewness and kurtosis
+print("Skewness: %f" % df_train['SalePrice'].skew())
+print("Kurtosis: %f" % df_train['SalePrice'].kurt())
+
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+plt.show()
+
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+plt.show()
+
+#box plot overallqual/saleprice
+var = 'OverallQual'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+plt.show()
+
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 2.5)
+plt.show();
+
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
+#dealing with missing data
+df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index,1)
+df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
+df_train.isnull().sum().max() #just checking that there's no missing data missing...
+
+#univariate analysis
+#standardizing data
+saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:,np.newaxis]);
+low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
+
+#bivariate analysis saleprice/grlivarea
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
