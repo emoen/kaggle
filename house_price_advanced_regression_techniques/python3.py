a271bdf76a58315751d324cbce666c2662df6aa4
emoen <endremoen@hotmail.com>
2017-10-08 19:21:21 +0200

    root means square error 0.16698

--- house_price_advanced_regression_techniques/python3.py
+++ house_price_advanced_regression_techniques/python3.py
@@ -17,74 +17,75 @@ df_test = pd.read_csv("test.csv")
 
 #missing data
 #delete em
-df_train = df_train.drop('PoolQC', 1)
-df_train = df_train.drop('MiscFeature', 1)
-df_train = df_train.drop('Alley', 1) 
-df_train = df_train.drop('Fence', 1)
-df_train = df_train.drop('FireplaceQu', 1)
-df_train = df_train.drop('LotFrontage', 1)
-df_train = df_train.drop('GarageType', 1)
-df_train = df_train.drop('GarageCond', 1)
-df_train = df_train.drop('GarageYrBlt', 1)
-df_train = df_train.drop('GarageFinish', 1)
-df_train = df_train.drop('GarageQual', 1)
-df_train = df_train.drop('BsmtExposure', 1)
-df_train = df_train.drop('BsmtFinTypeB', 1)    
-df_train = df_train.drop('BsmtFinTypeA', 1)    
-df_train = df_train.drop('BsmtCond', 1)    
-df_train = df_train.drop('BsmtQual', 1)    
-df_train = df_train.drop('MasVnrArea', 1) 
-df_train = df_train.drop('MasVnrType', 1)
-df_train = df_train.drop('Street', 1)      #not many datasets 
-df_train = df_train.drop('Utilities', 1)   #only 1 dataset
-df_train = df_train.drop('ConditionB', 1)   #only 14 dataset
-df_train = df_train.drop('OverallCond', 1) # is just some noise
+def preprocessing(df):
+    df = df.drop('PoolQC', 1)
+    df = df.drop('MiscFeature', 1)
+    df = df.drop('Alley', 1) 
+    df = df.drop('Fence', 1)
+    df = df.drop('FireplaceQu', 1)
+    df = df.drop('LotFrontage', 1)
+    df = df.drop('GarageType', 1)
+    df = df.drop('GarageCond', 1)
+    df = df.drop('GarageYrBlt', 1)
+    df = df.drop('GarageFinish', 1)
+    df = df.drop('GarageQual', 1)
+    df = df.drop('BsmtExposure', 1)
+    df = df.drop('BsmtFinTypeB', 1)    
+    df = df.drop('BsmtFinTypeA', 1)    
+    df = df.drop('BsmtCond', 1)    
+    df = df.drop('BsmtQual', 1)    
+    df = df.drop('MasVnrArea', 1) 
+    df = df.drop('MasVnrType', 1)
+    df = df.drop('Street', 1)      #not many datasets 
+    df = df.drop('Utilities', 1)   #only 1 dataset
+    df = df.drop('ConditionB', 1)   #only 14 dataset
+    df = df.drop('OverallCond', 1) # is just some noise
+    df.loc[df.MSZoning == 'C (all)', 'MSZoning'] = 'C'
+    df.loc[df.BldgType == '1Fam', 'BldgType'] = 'AFam'
+    df.loc[df.BldgType == '2fmCon', 'BldgType'] = 'BfmCon'
+    df.loc[df.HouseStyle == '2Story', 'HouseStyle'] = 'BStory'
+    df.loc[df.HouseStyle == '1Story', 'HouseStyle'] = 'AStory'
+    df.loc[df.HouseStyle == '1.5Fin', 'HouseStyle'] = 'ABFin'
+    df.loc[df.HouseStyle == '1.5Unf', 'HouseStyle'] = 'ABUnf'
+    df.loc[df.HouseStyle == '2.5Unf', 'HouseStyle'] = 'BCUnf'
+    df.loc[df.HouseStyle == '2.5Fin', 'HouseStyle'] = 'BCFin'
+    df.loc[df.OverallQual == 1, 'OverallQual'] = 'A'
+    df.loc[df.OverallQual == 2, 'OverallQual'] = 'B'
+    df.loc[df.OverallQual == 3, 'OverallQual'] = 'C'
+    df.loc[df.OverallQual == 4, 'OverallQual'] = 'D'
+    df.loc[df.OverallQual == 5, 'OverallQual'] = 'E'
+    df.loc[df.OverallQual == 6, 'OverallQual'] = 'F'
+    df.loc[df.OverallQual == 7, 'OverallQual'] = 'G'
+    df.loc[df.OverallQual == 8, 'OverallQual'] = 'H'
+    df.loc[df.OverallQual == 9, 'OverallQual'] = 'I'
+    df.loc[df.OverallQual == 10, 'OverallQual'] = 'J'
+    df.loc[df.RoofMatl == 'Tar&Grv', 'RoofMatl'] = 'Tar_Grv'
+    df.loc[df.ExteriorAst == 'Wd Sdng', 'ExteriorAst'] = 'Wd_Sdng'
+    df.loc[df.ExteriorBnd == 'Wd Shng', 'ExteriorBnd'] = 'Wd_Shng'
+    df.loc[df.ExteriorBnd == 'Wd Sdng', 'ExteriorBnd'] = 'Wd_Sdng'
+    df.loc[df.ExteriorBnd == 'Brk Cmn', 'ExteriorBnd'] = 'Brk_Cmn'
+    df.loc[df.ExteriorBnd == 'Min1', 'Functional'] = 'MinA'
+    df.loc[df.ExteriorBnd == 'Maj1', 'Functional'] = 'MajA'
+    df.loc[df.ExteriorBnd == 'Min2', 'Functional'] = 'MinB'
+    df.loc[df.ExteriorBnd == 'Maj2', 'Functional'] = 'MajB'
+    df.loc[df.LotConfig == 'FR2', 'LotConfig'] = 'FRA'
+    df.loc[df.LotConfig == 'FR3', 'LotConfig'] = 'FRB'
+    return df
+#df_train.MSZoning.unique()
 
- #looks unrelated. Can be included later
 
+df_train = preprocessing(df_train)
+df_test = preprocessing(df_test)
+
+#looks unrelated. Can be included later
 df_train = df_train.drop(df_train[df_train['Id'] == 1299].index)
 df_train = df_train.drop(df_train[df_train['Id'] == 524].index)
-
-df_train.loc[df_train.MSZoning == 'C (all)', 'MSZoning'] = 'C'
-df_train.loc[df_train.BldgType == '1Fam', 'BldgType'] = 'AFam'
-df_train.loc[df_train.BldgType == '2fmCon', 'BldgType'] = 'BfmCon'
-df_train.loc[df_train.HouseStyle == '2Story', 'HouseStyle'] = 'BStory'
-df_train.loc[df_train.HouseStyle == '1Story', 'HouseStyle'] = 'AStory'
-df_train.loc[df_train.HouseStyle == '1.5Fin', 'HouseStyle'] = 'ABFin'
-df_train.loc[df_train.HouseStyle == '1.5Unf', 'HouseStyle'] = 'ABUnf'
-df_train.loc[df_train.HouseStyle == '2.5Unf', 'HouseStyle'] = 'BCUnf'
-df_train.loc[df_train.HouseStyle == '2.5Fin', 'HouseStyle'] = 'BCFin'
-df_train.loc[df_train.OverallQual == 1, 'OverallQual'] = 'A'
-df_train.loc[df_train.OverallQual == 2, 'OverallQual'] = 'B'
-df_train.loc[df_train.OverallQual == 3, 'OverallQual'] = 'C'
-df_train.loc[df_train.OverallQual == 4, 'OverallQual'] = 'D'
-df_train.loc[df_train.OverallQual == 5, 'OverallQual'] = 'E'
-df_train.loc[df_train.OverallQual == 6, 'OverallQual'] = 'F'
-df_train.loc[df_train.OverallQual == 7, 'OverallQual'] = 'G'
-df_train.loc[df_train.OverallQual == 8, 'OverallQual'] = 'H'
-df_train.loc[df_train.OverallQual == 9, 'OverallQual'] = 'I'
-df_train.loc[df_train.OverallQual == 10, 'OverallQual'] = 'J'
-df_train.loc[df_train.RoofMatl == 'Tar&Grv', 'RoofMatl'] = 'Tar_Grv'
-df_train.loc[df_train.ExteriorAst == 'Wd Sdng', 'ExteriorAst'] = 'Wd_Sdng'
-df_train.loc[df_train.ExteriorBnd == 'Wd Shng', 'ExteriorBnd'] = 'Wd_Shng'
-df_train.loc[df_train.ExteriorBnd == 'Wd Sdng', 'ExteriorBnd'] = 'Wd_Sdng'
-df_train.loc[df_train.ExteriorBnd == 'Brk Cmn', 'ExteriorBnd'] = 'Brk_Cmn'
-df_train.loc[df_train.ExteriorBnd == 'Min1', 'Functional'] = 'MinA'
-df_train.loc[df_train.ExteriorBnd == 'Maj1', 'Functional'] = 'MajA'
-df_train.loc[df_train.ExteriorBnd == 'Min2', 'Functional'] = 'MinB'
-df_train.loc[df_train.ExteriorBnd == 'Maj2', 'Functional'] = 'MajB'
-df_train.loc[df_train.LotConfig == 'FR2', 'LotConfig'] = 'FRA'
-df_train.loc[df_train.LotConfig == 'FR3', 'LotConfig'] = 'FRB'
-#df_train.MSZoning.unique()
 
 #df_train.rename(index=str, columns={"Condition1": "ConditionA", "Exterior1st": "ExteriorAst", 
 #"Exterior2nd":"ExteriorBnd","BsmtFinSF1":"BsmtFinSFA","BsmtFinSF2":"BsmtFinSFB","1stFlrSF":"AstFlrSF","2ndFlrSF":"BndFlrSF","3SsnPorch":"CSsnPorch"})
 
-#Log transfering data
-all_data = pd.concat((df_train.loc[:,'MSSubClass':'SaleCondition'],
-                      df_test.loc[:,'MSSubClass':'SaleCondition']))
-
-df_train2 = pd.get_dummies(df_train)
+df_train2 = pd.get_dummies(df_train) 
+df_test2 = pd.get_dummies(df_test) 
 all_columns = " + ".join(df_train2[df_train2.columns.difference(['Id', 'SalePrice'])].columns)
 est = smf.ols(formula="SalePrice ~ "+all_columns, data=df_train2).fit()
 
@@ -92,12 +93,15 @@ est = smf.ols(formula="SalePrice ~ "+all_columns, data=df_train2).fit()
 #ttmp = pca.fit_transform(df_train2)
 
 #df_train2 = df_train2.copy
-df_train2['predicted'] = est.predict(df_train2) 
+missingCol = set(df_train2.columns) - set(df_test2.columns)
+for c in missingCol:
+    df_test2[c] = 0
+# Ensure the order of column in the test set is in the same order than in train set
+df_test2 = df_test2[df_train2.columns]
+df_test2['SalePrice'] = est.predict(df_test2) 
 #df_train2 = pd.get_dummies(df_train2)
-df_train2['predicted'] = df_train.predicted.round()
-tmp = df_train2.copy()
-tmp['Id'] += 1460
-tmp[['Id', 'predicted']].to_csv("reg.csv", index=False)
+
+df_test2[['Id', 'SalePrice']].to_csv("reg.csv", index=False)
 
 #df_train.columns.values[9] = 'ConditionA'
 #df_train.columns.values[17] = 'ExteriorAst'
