1886f773a6b9f4281104d2b972e520d2598216c4
emoen <endremoen@hotmail.com>
2017-10-31 07:14:27 +0100

    aggregating lessons learned - but still only at 0.125 - not better than previous model

--- house_price_advanced_regression_techniques/stackedModels2.py
+++ house_price_advanced_regression_techniques/stackedModels2.py
@@ -24,10 +24,27 @@ import lightgbm as lgb
 
 from sklearn.preprocessing import LabelEncoder
 
+from sklearn import ensemble, tree, linear_model
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.utils import shuffle
+
+from scipy.special import boxcox1p
+
+from scipy.stats import norm, skew #for some statistics
+
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
-features = pd.concat([train, test], keys=['train', 'test']) 
+#Out - liars!
+train = train.drop(train[train['Id'] == 1299].index)
+train = train.drop(train[train['Id'] == 524].index)
+
+#saleprice only in train set
+trainSalePrice = train.pop('SalePrice')
+
+features = pd.concat([train, test], keys=['train', 'test'])
+
+#features.loc['train'] 
 
 #drop cols
 features.drop(['Utilities', 'BsmtFinSFB', 'BsmtUnfSF', 'Heating', 'LowQualFinSF',
@@ -43,14 +60,133 @@ features.drop(['Utilities', 'BsmtFinSFB', 'BsmtUnfSF', 'Heating', 'LowQualFinSF'
 #delete: 'Utilities', 'LowQualFinSF', BsmtHalfBath
     
 dofillna()
-difNames(features)
+toCategorical()
+features['GarageCars'] = features['GarageCars'].astype(int)
+features['LotArea'] = features['LotArea'].astype(float)
+features['GrLivArea'] = features['GrLivArea'].astype(float)
 
-#Out - liars!
-features = features.drop(features[features['Id'] == 1299].index)
-features = features.drop(features[features['Id'] == 524].index)
+changeNamesBeforeDummy(features)
 
-df['SalePrice'] = np.log(df['SalePrice'])
-logTransf()
+#log transform (then standardize)
+trainSalePrice = np.log(trainSalePrice)
+#features['GrLivArea'] = np.log(features['GrLivArea'])
+
+#Standardize floats
+#features.loc[:,features.dtypes == np.float64].columns
+
+floatFeatures = features.loc[:,['BsmtFinSFA', 'GrLivArea', 'LotFrontage', 'LotArea', 'MasVnrArea', 'TotalSF']] 
+floatFeatures_standardized = (floatFeatures - floatFeatures.mean()) / floatFeatures.std()
+
+#ax = sns.pairplot(floatFeatures_standardized)
+
+labelencode(features)	
+features = dummiesDef(features)
+#features = pd.get_dummies(features) 
+
+doBoxCox(features)
+
+features_standardized = features.copy()
+features_standardized.update(floatFeatures_standardized)
+
+### Splitting features
+train_features = features.loc['train'].drop('Id', axis=1).select_dtypes(include=[np.number]).values
+test_features = features.loc['test'].drop('Id', axis=1).select_dtypes(include=[np.number]).values
+### Splitting standardized features
+train_features_st = features_standardized.loc['train'].drop('Id', axis=1).select_dtypes(include=[np.number]).values
+test_features_st = features_standardized.loc['test'].drop('Id', axis=1).select_dtypes(include=[np.number]).values
+
+# splitting
+### Shuffling train sets
+train_features_st, train_features, trainSalePrice = shuffle(train_features_st, train_features, trainSalePrice, random_state = 5)
+### Splitting
+x_train, x_test, y_train, y_test = train_test_split(train_features, trainSalePrice, test_size=0.1, random_state=200)
+x_train_st, x_test_st, y_train_st, y_test_st = train_test_split(train_features_st, trainSalePrice, test_size=0.1, random_state=200)
+
+#1st level models
+ENSTest = linear_model.ElasticNetCV(alphas=[0.0001, 0.0005, 0.001, 0.01, 0.1, 1, 10], l1_ratio=[.01, .1, .5, .9, .99], max_iter=5000).fit(x_train_st, y_train_st)
+train_test(ENSTest, x_train_st, x_test_st, y_train_st, y_test_st)
+
+scores = cross_val_score(ENSTest, train_features_st, trainSalePrice, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+#moaar base models
+lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10, 
+                                   loss='huber', random_state =5)
+		
+GBest = ensemble.GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05, max_depth=3, max_features='sqrt',
+        min_samples_leaf=15, min_samples_split=10, loss='huber').fit(x_train, y_train)	
+		
+model_xgb = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, 
+                             learning_rate=0.05, max_depth=3, 
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                              nthread = -1)
+model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
+                              learning_rate=0.05, n_estimators=720,
+                              max_bin = 55, bagging_fraction = 0.8,
+                              bagging_freq = 5, feature_fraction = 0.2319,
+                              feature_fraction_seed=9, bagging_seed=9,
+                              min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
+
+lassoEst = lasso.fit(x_train_st, y_train_st)
+train_test(lassoEst, x_train_st, x_test_st, y_train_st, y_test_st)	
+
+ENetEst = ENet.fit(x_train_st, y_train_st)
+train_test(ENetEst, x_train_st, x_test_st, y_train_st, y_test_st)	
+
+KRREst = KRR.fit(x_train_st, y_train_st)
+train_test(KRREst, x_train_st, x_test_st, y_train_st, y_test_st)	
+
+GBoostEst = GBoost.fit(x_train_st, y_train_st)
+train_test(GBoostEst, x_train_st, x_test_st, y_train_st, y_test_st)	
+
+GBestEst = GBest.fit(x_train_st, y_train_st)
+train_test(GBestEst, x_train_st, x_test_st, y_train_st, y_test_st)	
+
+model_xgbEst = model_xgb.fit(x_train_st, y_train_st)
+train_test(model_xgbEst, x_train_st, x_test_st, y_train_st, y_test_st)	
+
+model_lgbEst = model_lgb.fit(x_train_st, y_train_st)
+train_test(model_lgbEst, x_train_st, x_test_st, y_train_st, y_test_st)	
+
+scores = cross_val_score(lassoEst, train_features_st, trainSalePrice, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(ENetEst, train_features_st, trainSalePrice, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(KRREst, train_features_st, trainSalePrice, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(GBoostEst, train_features_st, trainSalePrice, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(model_xgbEst, train_features_st, trainSalePrice, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(model_lgbEst, train_features_st, trainSalePrice, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+#retrain models
+ll = lasso.fit(train_features_st, trainSalePrice)
+en = ENet.fit(train_features_st, trainSalePrice)
+kr = KRR.fit(train_features_st, trainSalePrice)
+mo1 = model_xgb.fit(train_features_st, trainSalePrice)
+mo2 = model_lgb.fit(train_features_st, trainSalePrice)
+
+#mo1 and mo2 way off... also off: 
+
+#tmpSaleP = ll.predict(test_features_st) + en.predict(test_features_st) + kr.predict(test_features_st) + mo1.predict(test_features_st) + mo2.predict(test_features_st)
+tmpSaleP = ll.predict(test_features_st) + en.predict(test_features_st) + kr.predict(test_features_st)
+#tmpSaleP =  ll.predict(test_features_st)
+tmpSaleP = tmpSaleP / 3
+salePrice = np.exp(tmpSaleP)
+
+out = pd.DataFrame()
+out['SalePrice'] = salePrice
+out['Id'] = test['Id']
+out[['Id', 'SalePrice']].to_csv("reg.csv", index=False)
 
 def dofillna():
     # MSSubClass as str
@@ -91,11 +227,22 @@ def dofillna():
     # Year and Month to categorical
     features['YrSold'] = features['YrSold'].astype(str)
     features['MoSold'] = features['MoSold'].astype(str)
+    #Adding basement feature
+    features['HasBsmt'] = pd.Series(len(features['TotalBsmtSF']), index=features.index)
+    features['HasBsmt'] = 0 
+    features.loc[features['TotalBsmtSF']>0,'HasBsmt'] = 1
     # Adding total sqfootage feature and removing Basement, 1st and 2nd floor features
     features['TotalSF'] = features['TotalBsmtSF'] + features['AstFlrSF'] + features['BndFlrSF']
     features.drop(['TotalBsmtSF', 'AstFlrSF', 'BndFlrSF'], axis=1, inplace=True)
+
+def toCategorical():
+    features['MSSubClass'] = features['MSSubClass'].astype(str)
+    features['YrSold'] = features['YrSold'].astype(str)
+    features['MoSold'] = features['MoSold'].astype(str)
+    features['HasBsmt'] = features['HasBsmt'].astype(str)
 	
-def difNames(df):
+	
+def changeNamesBeforeDummy(df):
     df.loc[df.MSZoning == 'C (all)', 'MSZoning'] = 'C'
     df.loc[df.BldgType == '1Fam', 'BldgType'] = 'AFam'
     df.loc[df.BldgType == '2fmCon', 'BldgType'] = 'BfmCon'
@@ -120,22 +267,113 @@ def difNames(df):
     df.loc[df.ExteriorBnd == 'Wd Shng', 'ExteriorBnd'] = 'Wd_Shng'
     df.loc[df.ExteriorBnd == 'Wd Sdng', 'ExteriorBnd'] = 'Wd_Sdng'
     df.loc[df.ExteriorBnd == 'Brk Cmn', 'ExteriorBnd'] = 'Brk_Cmn'
-    df.loc[df.ExteriorBnd == 'Min1', 'Functional'] = 'MinA'
-    df.loc[df.ExteriorBnd == 'Maj1', 'Functional'] = 'MajA'
-    df.loc[df.ExteriorBnd == 'Min2', 'Functional'] = 'MinB'
-    df.loc[df.ExteriorBnd == 'Maj2', 'Functional'] = 'MajB'
+#    df.loc[df.ExteriorBnd == 'Min1', 'Functional'] = 'MinA'
+#    df.loc[df.ExteriorBnd == 'Maj1', 'Functional'] = 'MajA'
+#    df.loc[df.ExteriorBnd == 'Min2', 'Functional'] = 'MinB'
+#    df.loc[df.ExteriorBnd == 'Maj2', 'Functional'] = 'MajB'
     df.loc[df.LotConfig == 'FR2', 'LotConfig'] = 'FRA'
     df.loc[df.LotConfig == 'FR3', 'LotConfig'] = 'FRB'	
 
-#Normality train/test - log transform
-def logTransfTrain(df)
-    df['GrLivArea'] = np.log(df_test2['GrLivArea'])
-    df['HasBsmt'] = pd.Series(len(df_test2['TotalBsmtSF']), index=df_test2.index)
-    df['HasBsmt'] = 0 
-    df.loc[df_test2['TotalBsmtSF']>0,'HasBsmt'] = 1
-	
+
+def dummiesDef(features):
+    # Getting Dummies from Condition1 and Condition2
+    conditions = set([x for x in features['ConditionA']] + [x for x in features['ConditionB']])
+    dummies = pd.DataFrame(data=np.zeros((len(features.index), len(conditions))),
+                       index=features.index, columns=conditions)
+    for i, cond in enumerate(zip(features['ConditionA'], features['ConditionB'])):
+        dummies.ix[i, cond] = 1
+    features = pd.concat([features, dummies.add_prefix('Condition_')], axis=1)
+    features.drop(['ConditionA', 'ConditionB'], axis=1, inplace=True)
+    
+    # Getting Dummies from Exterior1st and Exterior2nd
+    exteriors = set([x for x in features['ExteriorAst']] + [x for x in features['ExteriorBnd']])
+    dummies = pd.DataFrame(data=np.zeros((len(features.index), len(exteriors))),
+                       index=features.index, columns=exteriors)
+    for i, ext in enumerate(zip(features['ExteriorAst'], features['ExteriorBnd'])):
+        dummies.ix[i, ext] = 1
+    features = pd.concat([features, dummies.add_prefix('Exterior_')], axis=1)
+    features.drop(['ExteriorAst', 'ExteriorBnd', 'Exterior_nan'], axis=1, inplace=True)
+    
+    # Getting Dummies from all other categorical vars
+    for col in features.dtypes[features.dtypes == 'object'].index:
+        for_dummy = features.pop(col)
+        features = pd.concat([features, pd.get_dummies(for_dummy, prefix=col)], axis=1)	
+    return features
+
+# process columns, apply LabelEncoder to categorical features
+# Categorical variables that may contain information in their ordering set
+def labelencode(df):
+    cols = ('ExterQual', 'ExterCond','HeatingQC', 'KitchenQual', 'LandSlope',
+        'LotShape', 'PavedDrive', 'CentralAir', 'MSSubClass', 'OverallCond', 
+        'YrSold', 'MoSold')
+    for c in cols:
+        lbl = LabelEncoder() 
+        lbl.fit(list(df[c].values)) 
+        df[c] = lbl.transform(list(df[c].values))	
+		
+def doBoxCox(df):
+    #df_train3.drop("Id", axis = 1, inplace = True)
+    numeric_feats = df.dtypes[df.dtypes != "object"].index
+    numeric_feats = numeric_feats.drop("Id")
+    
+    skewed_feats = df[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+    skewness = pd.DataFrame({'Skew' :skewed_feats})
+    skewness = skewness[abs(skewness) > 0.75]
+    
+    skewed_features = skewness.index
+    lam = 0.15
+    for feat in skewed_features:
+        df[feat] = boxcox1p(df[feat], lam)
+
+##############################################################################################################################
+# Scoring
+
+# Prints R2 and RMSE scores
+def get_score(prediction, lables):    
+    print('R2: {}'.format(r2_score(prediction, lables)))
+    print('RMSE: {}'.format(np.sqrt(mean_squared_error(prediction, lables))))
+
+# Shows scores for train and validation sets    
+def train_test(estimator, x_trn, x_tst, y_trn, y_tst):
+    prediction_train = estimator.predict(x_trn)
+    # Printing estimator
+    print(estimator)
+    # Printing train scores
+    get_score(prediction_train, y_trn)
+    prediction_test = estimator.predict(x_tst)
+    # Printing test scores
+    print("Test")
+    get_score(prediction_test, y_tst)
 	
 ##############################################################################################################################
+
+#data type checking:
+
+#float 64:
+#train: 'LotFrontage', u'MasVnrArea', u'GarageYrBlt']
+#test:  'LotFrontage', u'MasVnrArea', u'GarageYrBlt', u'BsmtFinSFA', u'TotalBsmtSF',  u'', u'GarageCars'
+
+#int 64: 
+#>>> train.loc[:,train.dtypes == np.int64].columns
+#Index([u'Id', u'MSSubClass', u'LotArea', u'OverallQual', u'OverallCond',
+#       u'YearBuilt', u'YearRemodAdd', u'BsmtFinSFA', u'BsmtFinSFB',
+#       u'BsmtUnfSF', u'TotalBsmtSF', u'AstFlrSF', u'BndFlrSF', u'LowQualFinSF',
+#       u'GrLivArea', u'BsmtFullBath', u'BsmtHalfBath', u'FullBath',
+#       u'HalfBath', u'BedroomAbvGr', u'KitchenAbvGr', u'TotRmsAbvGrd',
+#       u'Fireplaces', u'GarageCars', u'GarageArea', u'WoodDeckSF',
+#       u'OpenPorchSF', u'EnclosedPorch', u'CSsnPorch', u'ScreenPorch',
+#       u'PoolArea', u'MiscVal', u'MoSold', u'YrSold'],
+	   
+#>>> test.loc[:,test.dtypes == np.int64].columns
+#Index([u'Id', u'MSSubClass', u'LotArea', u'OverallQual', u'OverallCond',
+#       u'YearBuilt', u'YearRemodAdd', u'AstFlrSF', u'BndFlrSF',
+#       u'LowQualFinSF', u'GrLivArea', u'FullBath', u'HalfBath',
+#       u'BedroomAbvGr', u'KitchenAbvGr', u'TotRmsAbvGrd', u'Fireplaces',
+#       u'WoodDeckSF', u'OpenPorchSF', u'EnclosedPorch', u'CSsnPorch',
+#       u'ScreenPorch', u'PoolArea', u'MiscVal', u'MoSold', u'YrSold'],	   
+
+#object equal
+
 #missing data
 total = features.isnull().sum().sort_values(ascending=False)
 percent = (features.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
