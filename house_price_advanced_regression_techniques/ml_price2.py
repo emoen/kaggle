5489edfe50660f260bca655d266a1d3bf1d61709
Moen Endre <endre.moen@imr.no>
2018-01-11 16:38:49 +0100

    dummy categorical data

--- house_price_advanced_regression_techniques/ml_price2.py
+++ house_price_advanced_regression_techniques/ml_price2.py
@@ -31,32 +31,41 @@ train = train.drop(train[train['Id'] == 1299].index)
 train = train.drop(train[train['Id'] == 524].index)
 
 features = pd.concat([train, test], keys=['train', 'test'])
+
+features['TotalBsmtSF'] = features['TotalBsmtSF'].fillna(0)
+features['TotalSF'] = features['TotalBsmtSF'] + features['AstFlrSF'] + features['BndFlrSF']
+
 trainFeatures = features.loc['train']
-simpleFeatures = trainFeatures.loc[:,['OverallQual']]
-simpleTestFeatures = features.loc['test'].loc[:,'OverallQual']
-#simpleFeatures = changeNamesBeforeDummy(simpleFeatures)
+simpleFeatures = trainFeatures.loc[:,['OverallQual', 'YearBuilt', 'TotalSF', 'GrLivArea']]
+
+simpleFeatures = changeNamesBeforeDummy(simpleFeatures)
+dummyTrain = pd.get_dummies( simpleFeatures )
+
+testFeatures = features.loc['test']
+simpleTestFeatures = testFeatures.loc[:,['OverallQual','YearBuilt', 'TotalSF', 'GrLivArea']]
+
+simpleTestFeatures = changeNamesBeforeDummy(simpleTestFeatures)
+dummyTest = pd.get_dummies( simpleTestFeatures )
+
 true_Y = train.loc[:,['SalePrice']]
 Y = np.log1p(true_Y)
 
-X_tr, X_val, y_tr, y_val = train_test_split(simpleFeatures, Y)
+#X_tr, X_val, y_tr, y_val = train_test_split(dummyTrain, Y)
 
 def baseline_model():
     # create model
     model = Sequential()
-    model.add(Dense(10, input_dim=1, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='normal'))
     # Compile model
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
     return model
     
 model = baseline_model()
-model.fit(X_tr,y_tr,validation_data=(X_val,y_val),epochs=30,batch_size=100)
+#model.fit(X_tr,y_tr,validation_data=(X_val,y_val),epochs=30,batch_size=100)
+model.fit(dummyTrain, Y, validation_split=0.15, epochs=30, batch_size=100)
 
-np.sqrt(model.evaluate(X_val,y_val))
+#np.sqrt(model.evaluate(X_val,y_val))
+preds = model.predict(dummyTest.values)
+np.expm1(preds.mean())
 
-preds = model.predict(simpleTestFeatures.values)
-
-print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
-
-estimator.fit(simpleFeatures, true_Y)
-pred_Y = estimator.predict(simpleFeatures)  #pred_Y - numpy.ndarray  
